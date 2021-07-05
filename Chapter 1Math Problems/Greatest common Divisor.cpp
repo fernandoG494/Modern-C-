fe8ec05019f8c3929c30413159d62e12b11fe282
fernandoG494 <darkogarcia3@gmail.com>
2021-07-05 17:40:23 -0500

    second problem

---
+++ Chapter 1Math Problems/Greatest common Divisor.cpp
@@ -0,0 +1,19 @@
+/*
+*   Coded by Incuerdo
+*   The Modern C++ Challenge: Become an expert programmer by solving real-world problems by Marius Bancilla
+*   Chapter 1: Math problems
+*       2.- Greatest common divisor:
+*       Write a program that, given two positive integers, will calculate and print the greatest common divisor
+*       of the two.    
+*/
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
